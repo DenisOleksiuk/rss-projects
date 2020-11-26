91399b0de21e7db0674192e4197f6fa847c5f3e0
Denis <DenisOleksiuk95@gmail.com>
2020-11-26 22:13:02 +0200

    feat: update CardData.js

--- English for kids/src/js/script.js
+++
@@ -1,8 +0,0 @@
-const humb = document.querySelector('.humb');
-const menu = document.querySelector('.menu');
-
-function active() {
-  humb.classList.toggle('humb__active');
-  menu.classList.toggle('menu__active');
-}
-humb.addEventListener('click', active);
---
+++ english-for-kids/src/js/cardData.js
@@ -0,0 +1,468 @@
+const cardData = {
+  verbs: [
+    {
+      image: 'assets/cards/verbs/force.png',
+      alt: 'force',
+      word: 'force',
+      translation: 'сила',
+      audioSrc: '"assets/mp3/verbs/force.mp3'
+    },
+    {
+      image: 'assets/cards/verbs/get.png',
+      alt: 'get',
+      word: 'get',
+      translation: 'получать',
+      audioSrc: 'asssets/mp3/verbs/get.mp3'
+    },
+    {
+      alt: 'guess',
+      word: 'guess',
+      translation: 'угадать',
+      image: 'assets/cards/verbs/guess.png',
+      audioSrc: 'assets/mp3/verbs/guess.mp3'
+    },
+    {
+      alt: 'increase',
+      word: 'increase',
+      translation: 'увеличиваться',
+      image: 'assets/cards/verbs/increase.png',
+      audioSrc: 'assets/mp3/verbs/increase.mp3'
+    },
+    {
+      alt: 'inform',
+      word: 'inform',
+      translation: 'сообщать',
+      image: 'assets/cards/verbs/inform.png',
+      audioSrc: 'assets/mp3/verbs/inform.mp3'
+    },
+    {
+      alt: 'keep',
+      word: 'keep',
+      translation: 'хранить',
+      image: 'assets/cards/verbs/keep.png',
+      audioSrc: 'assets/mp3/verbs/keep.mp3'
+    },
+    {
+      alt: 'lead',
+      word: 'lead',
+      translation: 'вести',
+      image: 'assets/cards/verbs/keep.png',
+      audioSrc: 'assets/mp3/verbs/guess.mp3'
+    },
+    {
+      word: 'reckon',
+      translation: 'рассчитывать',
+      image: 'assets/cards/verbs/reckon.png',
+      audioSrc: 'assets/mp3/verbs/reckon.mp3'
+    }
+  ],
+
+  adverbs: [
+    {
+      alt: 'absolutely',
+      word: 'absolutely',
+      translation: 'абсолютно',
+      image: 'assets/cards/adverbs/absolutely.jpg',
+      audioSrc: 'assets/mp3/adverbs/absolutely.mp3'
+    },
+    {
+      alt: 'almost',
+      word: 'almost',
+      translation: 'почти',
+      image: 'assets/cards/adverbs/almost.png',
+      audioSrc: 'assets/mp3/adverbs/almost.mp3'
+    },
+    {
+      alt: 'always',
+      word: 'always',
+      translation: 'всегда',
+      image: 'assets/cards/adverbs/always.png',
+      audioSrc: 'assets/mp3/adverbs/always.mp3'
+    },
+    {
+      alt: 'blindly',
+      word: 'blindly',
+      translation: 'слепо',
+      image: 'assets/cards/adverbs/blindly.png',
+      audioSrc: 'assets/mp3/adverbs/blindly.mp3'
+    },
+    {
+      alt: 'briefly',
+      word: 'briefly',
+      translation: 'кратко',
+      image: 'assets/cards/adverbs/briefly.png',
+      audioSrc: 'assets/mp3/adverbs/briefly.mp3'
+    },
+    {
+      alt: 'sinbroadlyg',
+      word: 'sinbroadlyg',
+      translation: 'петь',
+      image: 'assets/cards/adverbs/broadly.png',
+      audioSrc: 'assets/mp3/adverbs/broadly.mp3'
+    },
+    {
+      alt: 'carefully',
+      word: 'carefully',
+      translation: 'внимательно',
+      image: 'assets/cards/adverbs/carefully.png',
+      audioSrc: 'assets/mp3/adverbs/carefully.mp3'
+    },
+    {
+      alt: 'certainly',
+      word: 'certainly',
+      translation: 'безусловно',
+      image: 'assets/cards/adverbs/certainly.png',
+      audioSrc: 'assets/mp3/adverbs/certainly.mp3'
+    }
+  ],
+  adjectives: [
+    {
+      alt: 'awful',
+      word: 'awful',
+      translation: 'ужасный',
+      image: 'assets/cards/adjectives/awful.png',
+      audioSrc: 'assets/mp3/adjectives/awful.mp3'
+    },
+    {
+      alt: 'popular',
+      word: 'popular',
+      translation: 'популярный',
+      image: 'assets/cards/adjectives/popular.png',
+      audioSrc: 'assets/mp3/adjectives/popular.mp3'
+    },
+    {
+      alt: 'possible',
+      word: 'possible',
+      translation: 'возможно',
+      image: 'assets/cards/adjectives/possible.jpg',
+      audioSrc: 'assets/mp3/adjectives/possible.mp3'
+    },
+    {
+      alt: 'private',
+      word: 'private',
+      translation: 'приватный',
+      image: 'assets/cards/adjectives/private.png',
+      audioSrc: 'assets/mp3/adjectives/private.mp3'
+    },
+    {
+      alt: 'recent',
+      word: 'recent',
+      translation: 'недавний',
+      image: 'assets/cards/adjectives/recent.png',
+      audioSrc: 'assets/mp3/adjectives/recent.mp3'
+    },
+    {
+      alt: 'responsible',
+      word: 'responsible',
+      translation: 'ответственный',
+      image: 'assets/cards/adjectives/responsible.png',
+      audioSrc: 'assets/mp3/adjectives/responsible.mp3'
+    },
+    {
+      alt: 'safe',
+      word: 'safe',
+      translation: 'безопасный',
+      image: 'assets/cards/adjectives/safe.png',
+      audioSrc: 'assets/mp3/adjectives/safe.mp3'
+    },
+    {
+      alt: 'same',
+      word: 'same',
+      translation: 'тот же',
+      image: 'assets/cards/adjectives/same.png',
+      audioSrc: 'assets/mp3/adjectives/same.mp3'
+    }
+  ],
+  preposition: [
+    {
+      alt: 'About',
+      word: 'About',
+      translation: 'около',
+      image: 'assets/cards/preposition/About.png',
+      audioSrc: 'assets/mp3/preposition/About.mp3'
+    },
+    {
+      alt: 'above',
+      word: 'above',
+      translation: 'над',
+      image: 'assets/cards/preposition/above.png',
+      audioSrc: 'assets/mp3/preposition/above.mp3'
+    },
+    {
+      alt: 'according to',
+      word: 'according to',
+      translation: 'в соответствии с',
+      image: 'assets/cards/preposition/according-to.png',
+      audioSrc: 'assets/mp3/preposition/according-to.mp3'
+    },
+    {
+      alt: 'across',
+      word: 'across',
+      translation: 'через',
+      image: 'assets/cards/preposition/across.png',
+      audioSrc: 'assets/mp3/preposition/across.mp3'
+    },
+    {
+      alt: 'against',
+      word: 'against',
+      translation: 'против',
+      image: 'assets/cards/preposition/against.png',
+      audioSrc: 'assets/mp3/preposition/against.mp3'
+    },
+    {
+      alt: 'against',
+      word: 'against',
+      translation: 'ответственный',
+      image: 'assets/cards/preposition/against.png',
+      audioSrc: 'assets/mp3/preposition/against.mp3'
+    },
+    {
+      alt: 'along',
+      word: 'along',
+      translation: 'вдоль',
+      image: 'assets/cards/preposition/along.png',
+      audioSrc: 'assets/mp3/preposition/along.mp3'
+    },
+    {
+      alt: 'although',
+      word: 'although',
+      translation: 'несмотря на то что',
+      image: 'assets/cards/preposition/although.png',
+      audioSrc: 'assets/mp3/preposition/although.mp3'
+    }
+  ],
+  itTerms: [
+    {
+      alt: 'adapt',
+      word: 'adapt',
+      translation: 'адаптировать',
+      image: 'assets/cards/it-terms/adapt.png',
+      audioSrc: 'assets/mp3/it-terms/adapt.mp3'
+    },
+    {
+      alt: 'archive',
+      word: 'archive',
+      translation: 'архив',
+      image: 'assets/cards/it-terms/archive.png',
+      audioSrc: 'assets/mp3/it-terms/archive.mp3'
+    },
+    {
+      alt: 'attempt',
+      word: 'attempt',
+      translation: 'попытка',
+      image: 'assets/cards/it-terms/attempt.pngit-terms',
+      audioSrc: 'assets/mp3/it-terms/attempt.mp3'
+    },
+    {
+      alt: 'binary',
+      word: 'binary',
+      translation: 'двоичный',
+      image: 'assets/cards/it-terms/binary.png',
+      audioSrc: 'assets/mp3/it-terms/binary.mp3'
+    },
+    {
+      alt: 'buffer',
+      word: 'buffer',
+      translation: 'буфер',
+      image: 'assets/cards/it-terms/buffer.png',
+      audioSrc: 'assets/mp3/it-terms/buffer.mp3'
+    },
+    {
+      alt: 'cache',
+      word: 'cache',
+      translation: 'кэш',
+      image: 'assets/cards/it-terms/cache.png',
+      audioSrc: 'assets/mp3/it-terms/cache.mp3'
+    },
+    {
+      alt: 'cancel',
+      word: 'cancel',
+      translation: 'отмена',
+      image: 'assets/cards/it-terms/cancel.png',
+      audioSrc: 'assets/mp3/it-terms/cancel.mp3'
+    },
+    {
+      alt: 'capacity',
+      word: 'capacity',
+      translation: 'емкость',
+      image: 'assets/cards/it-terms/capacity.png',
+      audioSrc: 'assets/mp3/it-terms/capacity.mp3'
+    }
+  ],
+  vegetables: [
+    {
+      alt: 'anise',
+      word: 'anise',
+      translation: 'анис',
+      image: 'assets/cards/vegetables/anise.png',
+      audioSrc: 'assets/mp3/vegetables/anise.mp3'
+    },
+    {
+      alt: 'artichoke',
+      word: 'artichoke',
+      translation: 'архив',
+      image: 'assets/cards/vegetables/artichoke.png',
+      audioSrc: 'assets/mp3/vegetables/artichoke.mp3'
+    },
+    {
+      alt: 'arugula',
+      word: 'arugula',
+      translation: 'руккола',
+      image: 'assets/cards/vegetables/arugula.png',
+      audioSrc: 'assets/mp3/vegetables/arugula.mp3'
+    },
+    {
+      alt: 'asparagus',
+      word: 'asparagus',
+      translation: 'спаржа',
+      image: 'assets/cards/vegetables/asparagus.png',
+      audioSrc: 'assets/mp3/vegetables/asparagus.mp3'
+    },
+    {
+      alt: 'avocado',
+      word: 'avocado',
+      translation: 'авакадо',
+      image: 'assets/cards/vegetables/avocado.png',
+      audioSrc: 'assets/mp3/vegetables/avocado.mp3'
+    },
+    {
+      alt: 'basil',
+      word: 'basil',
+      translation: 'базилик',
+      image: 'assets/cards/vegetables/basil.png',
+      audioSrc: 'assets/mp3/vegetables/basil.mp3'
+    },
+    {
+      alt: 'beans',
+      word: 'beans',
+      translation: 'фасоль',
+      image: 'assets/cards/vegetables/beans.png',
+      audioSrc: 'assets/mp3/vegetables/beans.mp3'
+    },
+    {
+      alt: 'bean-sprouts',
+      word: 'bean-sprouts',
+      translation: 'емкость',
+      image: 'assets/cards/vegetables/bean-sprouts.png',
+      audioSrc: 'assets/mp3/vegetables/bean-sprouts.mp3'
+    }
+  ],
+  collision: [
+    {
+      alt: 'behavior',
+      word: 'behavior',
+      translation: 'анис',
+      image: 'assets/cards/collision/behavior.png',
+      audioSrc: 'assets/mp3/collision/behavior.mp3'
+    },
+    {
+      alt: 'bounce',
+      word: 'bounce',
+      translation: 'подпрыгивать',
+      image: 'assets/cards/collision/bounce.png',
+      audioSrc: 'assets/mp3/collision/bounce.mp3'
+    },
+    {
+      alt: 'collision-process',
+      word: 'collision-process',
+      translation: 'процесс столкновения',
+      image: 'assets/cards/collision/collision-process.png',
+      audioSrc: 'assets/mp3/collision/collision-process.mp3'
+    },
+    {
+      alt: 'combination',
+      word: 'combination',
+      translation: 'сочетание',
+      image: 'assets/cards/collision/combination.png',
+      audioSrc: 'assets/mp3/collision/combination.mp3'
+    },
+    {
+      alt: 'concept',
+      word: 'concept',
+      translation: 'концепция',
+      image: 'assets/cards/collision/concept.png',
+      audioSrc: 'assets/mp3/collision/concept.mp3'
+    },
+    {
+      alt: 'cradle',
+      word: 'cradle',
+      translation: 'колыбель',
+      image: 'assets/cards/collision/cradle.png',
+      audioSrc: 'assets/mp3/collision/cradle.mp3'
+    },
+    {
+      alt: 'doubling',
+      word: 'doubling',
+      translation: 'удвоение',
+      image: 'assets/cards/collision/doubling.png',
+      audioSrc: 'assets/mp3/collision/doubling.mp3'
+    },
+    {
+      alt: 'elastic-collision',
+      word: 'elastic-collision',
+      translation: 'упругое столкновение',
+      image: 'assets/cards/collision/elastic-collision.png',
+      audioSrc: 'assets/mp3/collision/elastic-collision.mp3'
+    }
+  ],
+  bones: [
+    {
+      alt: 'bone Marrow',
+      word: 'bone Marrow',
+      translation: 'Костный мозг',
+      image: 'assets/cards/bones/bone-Marrow.png',
+      audioSrc: 'assets/mp3/bones/bone-Marrow.mp3'
+    },
+    {
+      alt: 'carpals',
+      word: 'carpals',
+      translation: 'запястья',
+      image: 'assets/cards/bones/carpals.png',
+      audioSrc: 'assets/mp3/bones/carpals.mp3'
+    },
+    {
+      alt: 'clavicle',
+      word: 'clavicle',
+      translation: 'ключица',
+      image: 'assets/cards/bones/clavicle.png',
+      audioSrc: 'assets/mp3/bones/clavicle.mp3'
+    },
+    {
+      alt: 'coccyx',
+      word: 'coccyx',
+      translation: 'копчик',
+      image: 'assets/cards/bones/coccyx.png',
+      audioSrc: 'assets/mp3/bones/coccyx.mp3'
+    },
+    {
+      alt: 'femur',
+      word: 'femur',
+      translation: 'бедренная кость',
+      image: 'assets/cards/bones/femur.png',
+      audioSrc: 'assets/mp3/bones/femur.mp3'
+    },
+    {
+      alt: 'human tissue',
+      word: 'human tissue',
+      translation: 'человеческая ткань',
+      image: 'assets/cards/bones/human-tissue.png',
+      audioSrc: 'assets/mp3/bones/human-tissue.mp3'
+    },
+    {
+      alt: 'humerus',
+      word: 'humerus',
+      translation: 'плечевая кость',
+      image: 'assets/cards/bones/humerus.png',
+      audioSrc: 'assets/mp3/bones/humerus.mp3'
+    },
+    {
+      alt: 'fibula',
+      word: 'fibula',
+      translation: 'малоберцовая кость',
+      image: 'assets/cards/bones/fibula.png',
+      audioSrc: 'assets/mp3/bones/fibula.mp3'
+    }
+  ]
+};
+
+export { cardData };
