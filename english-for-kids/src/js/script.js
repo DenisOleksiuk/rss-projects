b739fa68c7e4d0abb78e78c0a397c435bd9c665c
Denis <DenisOleksiuk95@gmail.com>
2020-11-29 18:29:00 +0200

    feat: add gulp and webpack setting

--- english-for-kids/src/js/script.js
+++ english-for-kids/src/js/script.js
@@ -3,44 +3,41 @@ import { WordCard } from './wordCard.js';
 
 const humb = document.querySelector('.humb');
 const menu = document.querySelector('.menu');
-const cardsAll = document.querySelector('.cards');
+const cardsParent = document.querySelector('.cards');
 const category = document.querySelectorAll('.category');
-const rotate = document.querySelectorAll('.card__rotate--btn');
 
-const adjective = new Audio('assets/mp3/adjectives/awful.mp3');
+// const adjective = new Audio('assets/mp3/adjectives/awful.mp3');
 
 function active() {
   humb.classList.toggle('humb__active');
   menu.classList.toggle('menu__active');
 }
 
-function rotateBtn(n) {
-  const parent = n.target.closest('.card__inner');
-  parent.classList.toggle('card__rotate');
-  adjective.play();
-}
+// function rotateBtn(n) {
+//   const parent = n.target.closest('.card__inner');
+//   parent.classList.toggle('card__rotate');
+//   adjective.play();
+// }
 
-function getTargetCard(card) {
+function getIdCard(card) {
   return card.target.closest('.category').id;
 }
 
-function switchCards(id) {
+function switchCards(cardElement) {
+  if (cardElement.target === cardsParent) return;
   category.forEach((mainCard) => {
     mainCard.setAttribute('hidden', '');
   });
 
-  for (let i = 0; i < cardData[getTargetCard(id)].length; i += 1) {
-    const cardObj = cardData[getTargetCard(id)];
+  for (let i = 0; i < cardData[getIdCard(cardElement)].length; i += 1) {
+    const cardObj = cardData[getIdCard(cardElement)];
     const card = new WordCard(document.querySelector('.cards'), cardObj[i]);
     card.render();
+    card.rotateCard('cardElement');
   }
 
-  cardsAll.removeEventListener('click', switchCards);
+  cardsParent.removeEventListener('click', switchCards);
 }
 
 humb.addEventListener('click', active);
-cardsAll.addEventListener('click', switchCards);
-
-rotate.forEach(items => {
-  items.addEventListener('click', rotateBtn);
-});
+cardsParent.addEventListener('click', switchCards);
--- english-for-kids/webpack.config.js
+++ english-for-kids/webpack.config.js
@@ -1,10 +1,30 @@
-const path = require('path');
+let path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/js/script.js',
   output: {
-    filename: './bundle.js',
-    path: path.resolve(__dirname, './dist')
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+              debug: true,
+              corejs: 3,
+              useBuiltIns: 'usage'
+            }]]
+          }
+        }
+      }
+    ]
   }
 };
